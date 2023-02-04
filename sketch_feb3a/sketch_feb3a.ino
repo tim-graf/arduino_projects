aaf61e31f1a5ff79e5ac9f736936387364575144
Tim Graf <tim.graf@gmail.com>
2023-02-04 23:17:13 +0100

    rewrite traffic lights as oop, synchronize two traffic lights

--- sketch_feb3a/sketch_feb3a.ino
+++ sketch_feb3a/sketch_feb3a.ino
@@ -1,83 +1,140 @@
-int red_pin = 11;
-int yellow_pin = 10;
-int green_pin = 9;
 
-int fg_red_pin = 5;
-int fg_green_pin = 6;
 
 int base_duration = 3000;
 int transition_duration = 1000;
 
-void light_setting(bool red, bool yellow, bool green){
+class TrafficLight {
+private:
+  int red_pin;
+  int yellow_pin;
+  int green_pin; 
+  int base_duration;
+  int transition_duration;
+  void init() {
+    pinMode(red_pin, OUTPUT);
+    pinMode(yellow_pin, OUTPUT);
+    pinMode(green_pin, OUTPUT);
+  };
+  void light_setting(bool red, bool yellow, bool green) {
+    if (red) {
+      digitalWrite(red_pin, HIGH);
+    } else {
+      digitalWrite(red_pin, LOW);
+    };
+    if (yellow) {
+      digitalWrite(yellow_pin, HIGH);
+    } else {
+      digitalWrite(yellow_pin, LOW);
+    };
+    if (green) {
+      digitalWrite(green_pin, HIGH);
+    } else {
+      digitalWrite(green_pin, LOW);
+    };
+  };
+
+  void red_phase() {
+    light_setting(true, false, false);
+    //delay(base_duration);
+  };
+
+  void red_to_green() {
+    light_setting(true, true, false);
+    //delay(transition_duration);
+  };
+
+  void green_phase() {
+    light_setting(false, false, true);
+    //delay(base_duration);
+  };
+
+  void green_to_red() {
+    light_setting(false, true, false);
+    //delay(transition_duration);
+  };
+public:
+  TrafficLight(int red_pin, int yellow_pin, int green_pin, int base_duration, int transition_duration, bool is_master=true) {
+    this->red_pin = red_pin;
+    this->yellow_pin = yellow_pin;
+    this->green_pin = green_pin;
+    this->base_duration = base_duration;
+    this->transition_duration = transition_duration;
+    this->is_master=is_master;
+    init();
+  };
+
+  bool is_master;  
+ 
+
+  void phases (int ii){
+    switch (ii) {
+      case 1:
+        red_phase();
+        break;
+      case 2:
+        red_to_green();
+        break;
+      case 3:
+        green_phase();
+        break;
+      case 4:
+        green_to_red();
+        break;
+        };
+    };
   
-  if (red) {
-    digitalWrite(red_pin, HIGH);
-  } else {
-    digitalWrite(red_pin, LOW);
-  };
-  if (yellow) {
-    digitalWrite(yellow_pin, HIGH);
-  } else {
-    digitalWrite(yellow_pin, LOW);
-  };
-  if (green) {
-    digitalWrite(green_pin, HIGH);
-  } else {
-    digitalWrite(green_pin, LOW);
-  };
-};
 
+  
 
-void fg_light_setting(bool red, bool green){
-  if (red) {
-    digitalWrite(fg_red_pin, HIGH);
-  } else {
-    digitalWrite(fg_red_pin, LOW);
-  };
+  void on() {
 
-  if (green) {
-    digitalWrite(fg_green_pin, HIGH);
-  } else {
-    digitalWrite(fg_green_pin, LOW);
+  for (int ii = 1; ii < 5; ii++){
+    int kk[4] = {3,4, 1, 2};
+    int jj;
+    if (is_master) {jj = ii;} else {jj = kk[ii-1];};
+    phases(jj);
+    if (ii % 2 != 0){
+      delay(base_duration);
+    } else {
+      delay(transition_duration);
+    };
   };
 };
-
-void red_phase (int green_red_time){
-  light_setting(true, false, false);
-  fg_light_setting(false, true);
-  delay(green_red_time);
 };
 
-void red_to_green (int yellow_time){
-  light_setting(true,true,false);
-  delay(yellow_time);
-};
-
-void green_phase (int green_red_time){
-  light_setting(false, false, true);
-  fg_light_setting(true, false);
-  delay(green_red_time);
-};
-
-void green_to_red(int yellow_time) {
-light_setting(false, true, false);
-delay(yellow_time);
-};
-
+TrafficLight masterTl(11,10,9, base_duration, transition_duration);
+TrafficLight slaveTl(5,6,7, base_duration, transition_duration,false);
 
 void setup() {
   // put your setup code here, to run once:
-pinMode(red_pin, OUTPUT);
-pinMode(yellow_pin, OUTPUT);
-pinMode(green_pin, OUTPUT);
-pinMode(fg_red_pin, OUTPUT);
-pinMode(fg_green_pin, OUTPUT);
+  // pinMode(red_pin, OUTPUT);
+  // pinMode(yellow_pin, OUTPUT);
+  // pinMode(green_pin, OUTPUT);
+  // pinMode(fg_red_pin, OUTPUT);
+  // pinMode(fg_green_pin, OUTPUT);
 }
+
+
+void crossing (TrafficLight master, TrafficLight slave){
+    for (int ii = 1; ii < 5; ii++){
+    int kk[4] = {3, 4, 1, 2};
+    master.phases(ii);
+    slave.phases(kk[ii-1]);
+    if (ii % 2 != 0){
+      delay(base_duration);
+    } else {
+      delay(transition_duration);
+    };
+  };
+};
+
+
 
 void loop() {
   // put your main code here, to run repeatedly:
-red_phase(base_duration);
-red_to_green(transition_duration);
-green_phase(base_duration);
-green_to_red(transition_duration);
+  // red_phase(base_duration);
+  // red_to_green(transition_duration);
+  // green_phase(base_duration);
+  // green_to_red(transition_duration);
+  crossing(masterTl,slaveTl);
 }
